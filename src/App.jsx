a9a908544d2bde27afcec8624ffb3f5abc80a4a1
J. Jami <144722335+JannatulJami@users.noreply.github.com>
2025-10-27 13:14:57 +0000

    pikpikpk

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+
+
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 
-function App() {
+function Home() {
   return (
-    <div>
-      <Login />
+    <div className="home-container">
+      <h1>Welcome to Unipatnpx</h1>
+      <p>Your path to SAT & AP success.</p>
+      <Link to="/login">
+        <button>Go to Login</button>
+      </Link>
     </div>
   );
 }
 
-export default App; 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
